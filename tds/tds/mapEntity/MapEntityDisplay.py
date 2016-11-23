56f103c9af12f39aa0e11d38799be65f6f4d56ce
Daniel Zuberbuehler <daniel.zuberbuehler@outlook.com>
2016-11-23 19:10:07 +0100

    corrected rotation

--- tds/tds/mapEntity/MapEntityDisplay.py
+++ tds/tds/mapEntity/MapEntityDisplay.py
@@ -4,4 +4,4 @@ from base.displayHandler import DisplayHandler
 
 class MapEntityDisplay(DisplayHandler):
 	def display(self):
-		return pygame.transform.rotozoom(super().display(), self.controller.angle.angle_to([0,0]), 1)
+		return pygame.transform.rotozoom(super().display(), self.controller.angle.angle_to([0, -1]), 1)
