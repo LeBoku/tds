87ac1fb71ba7a1343062fda13c50509373ea8644
Daniel Zuberbuehler <daniel.zuberbuehler@outlook.com>
2016-11-29 20:30:23 +0100

    imporved movement

--- tds/tds/mapEntity/player/playerController.py
+++ tds/tds/mapEntity/player/playerController.py
@@ -4,20 +4,52 @@ from pygame.math import Vector2
 
 from mapEntity.character.characterController import CharacterController
 
-from pygameUtil.EventHandling import EventListener
+from pygameUtil.EventHandling import EventListener, EventHandler
 from pygameUtil import math_
 
 class PlayerController(CharacterController):
 	def __init__(self, map):
 		super().__init__(map)
 		self.speed = 5
+		self.eventHandler = EventHandler.get()
+
+		self.movementKeys = {
+			pygame.locals.K_w: Vector2(0,-1),
+			pygame.locals.K_d: Vector2(1,0),
+			pygame.locals.K_s: Vector2(0,1),
+			pygame.locals.K_a: Vector2(-1,0)
+		}
 
 	def display(self):
 		self.updateAngle()
+		self.updateMovement()
 
 		print(self.isCollidingWithSomething())
 
 		return super().display()
+
+	def updateMovement(self):
+		pressedKeys = self.eventHandler.orderKeysByLoopsDown(self.movementKeys.keys())
+
+		if len(pressedKeys) > 0:
+			forwardMovement = Vector2(0,0)
+			self.startMoveAnimation()
+			for key in pressedKeys:
+				forwardMovement += self.movementKeys[key]
+				if not forwardMovement.length() == 0:
+					forwardMovement.normalize()
+
+			if forwardMovement.length() > 0:
+				forwardMovement.scale_to_length(self.speed)
+				self.coord += forwardMovement
+
+			
+			
+
+	def startMoveAnimation(self):
+		move = self.moveSetController.getMove("move")
+		if not move.isActive:
+			move.start()
 
 	def updateAngle(self):
 		mousePosVector = Vector2(pygame.mouse.get_pos())
@@ -25,9 +57,9 @@ class PlayerController(CharacterController):
 
 	def registerEvents(self):
 		super().registerEvents()
-		self.registerMovement()
+		self.registerCombat()
 
-	def registerMovement(self):
+	def registerCombat(self):
 		qWindow = 20
 	
 		@EventListener(pygame.locals.MOUSEBUTTONDOWN, button=1)
@@ -37,35 +69,3 @@ class PlayerController(CharacterController):
 				move.start()
 			elif move.framesLeft < qWindow:
 				move.listenForEnd(lambda: move.start())
-			
-		@EventListener(pygame.locals.KEYDOWN)
-		def li(event):
-			self.moveSetController.getMove("move").startLoop()
-
-		@EventListener(pygame.locals.KEYUP)
-		def li(event):
-			self.moveSetController.getMove("move").stop()
-
-		@EventListener("KEY_IS_DOWN", key=pygame.locals.K_w)
-		def li(event):
-			movement = Vector2(0, -5)
-			movement.scale_to_length(5)
-			self.coord += movement
-
-		@EventListener("KEY_IS_DOWN", key=pygame.locals.K_d)
-		def li(event):
-			movement = Vector2(5, 0)
-			movement.scale_to_length(5)
-			self.coord += movement
-
-		@EventListener("KEY_IS_DOWN", key=pygame.locals.K_s)
-		def li(event):
-			movement = Vector2(0, 5)	
-			movement.scale_to_length(5)
-			self.coord += movement
-	
-		@EventListener("KEY_IS_DOWN", key=pygame.locals.K_a)
-		def li(event):
-			movement = Vector2(-5, 0)
-			movement.scale_to_length(5)
-			self.coord += movement
