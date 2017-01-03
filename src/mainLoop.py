edbf12ffe2073d3b12d325f177cacb392b190ecb
Daniel Zuberbuehler <daniel.zuberbuehler@outlook.com>
2017-01-03 15:09:38 +0100

    switched from vs

--- src/mainLoop.py
+++ src/mainLoop.py
@@ -2,9 +2,7 @@ import sys
 
 import pygame
 
-import pygameUtil
 from pygameUtil.eventHandling import EventHandler, EventListener
-
 from map.map.mapController import MapController
 
 import config 
@@ -17,18 +15,20 @@ eventHandler = EventHandler.get()
 
 map = MapController()
 
-def mainLoop():
-	while isRunning:
-		eventHandler.postCustomEvents()
-		eventHandler.handleEvents()
 
-		mapDisplay = map.display()
-		
-		clock.tick(config.fps)
-		display.blit(mapDisplay, (0,0))
-		pygame.display.update()
+def mainLoop():
+    while isRunning:
+        eventHandler.postCustomEvents()
+        eventHandler.handleEvents()
+
+        mapDisplay = map.display()
+
+        clock.tick(config.fps)
+        display.blit(mapDisplay, (0,0))
+        pygame.display.update()
+
 
 @EventListener(pygame.locals.QUIT)
 def onClose(event):
-	pygame.quit()
-	sys.exit()
+    pygame.quit()
+    sys.exit()
